99c4e81f7483750afe45fecf8e021a6dff6fa66c
AbhishekRana <abhishekrana201005@gmail.com>
2022-02-28 21:24:48 +0530

    complteed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React,{useState} from 'react';
 import AddUser from './components/User/user';
+import UserList from './components/User/UserList';
 
 function App() {
+  const[userList,setUserList]=useState([]);
+  const addUserHandler1=(uname,uage)=>{
+    setUserList((...prevUserList)=>{
+      return [...prevUserList,{name:uname,age:uage,id:Math.random().toString}];
+    })
+  }
   return (
     <div>
-      <AddUser/>
+      <AddUser onAddUser={addUserHandler1}/>
+      <UserList users={userList}/>
     </div>
   );
 }
